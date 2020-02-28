1e91dd3eebabd1c462b453d11017ab4ce4681640
Pierre CURZOLA <pierre-gilles.curzola@epitech.eu>
2020-02-28 11:27:02 +0100

    config file with checker + github api url

--- server_config.go
+++ server_config.go
@@ -2,34 +2,70 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io/ioutil"
-	"log"
+	"net/url"
+	"strings"
+	"sync"
 )
 
-// APIToken personal access token to increase limit rate
-// will be use in request's header to github API
-var APIToken string
-
 // ServerConfig structure of server.config.json file
+// PersonalToken APIToken personal access token to increase limit rate
+// 	will be use in request's header to github API
 type ServerConfig struct {
+	GithubAPIURL  string `json:"github_api_url"`
 	PersonalToken string `json:"personal_token"`
 }
 
-func loadConfigFile() {
-	file, err := ioutil.ReadFile("server.config.json")
+// will load config file into path parameter
+// If no file found no error will be returned, project can work without
+func loadConfigFile(path string) (*ServerConfig, error) {
+	file, err := ioutil.ReadFile(path)
 
 	data := ServerConfig{}
 
 	if err != nil {
-		return
+		return nil, err
 	}
 
 	err = json.Unmarshal([]byte(file), &data)
 
 	if err != nil {
-		log.Println("Error during loading config file:", err)
-		return
+		return nil, err
 	}
 
-	APIToken = data.PersonalToken
+	return &data, nil
+}
+
+// Check will check conformity of config file
+func (env *ServerConfig) Check() error {
+	var errorMessages []string
+	errChan := make(chan error)
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	go checkURL(wg, errChan, env.GithubAPIURL, "github_api_url")
+
+	go func() {
+		wg.Wait()
+		close(errChan)
+	}()
+
+	for err := range errChan {
+		errorMessages = append(errorMessages, err.Error())
+	}
+
+	if len(errorMessages) > 0 {
+		return errors.New("  → " + strings.Join(errorMessages, "\n  → "))
+	}
+	return nil
+}
+
+func checkURL(wg *sync.WaitGroup, c chan error, raw string, name string) {
+	defer wg.Done()
+	_, err := url.Parse(raw)
+	if err != nil {
+		c <- fmt.Errorf("%v is not a valid URL: %v", name, err)
+	}
 }
