b4c94275d30b03e30411390085c03a2e6fd43373
Pierre CURZOLA <pierre-gilles.curzola@epitech.eu>
2020-02-29 10:16:28 +0100

    clean up

--- aggregation.go
+++ aggregation.go
@@ -14,66 +14,10 @@ type result struct {
 type languageStats struct {
 	count    uint16
 	size     uint64
-	repoList map[string]Repository // map[fullname: string]URL: string
-}
-
-// Pipeline:
-// 2 stages
-// 1st: list repo to chan
-// 2nd:
-//		- fetch languages
-// 		- aggregate repo: map[language: string]{count: number, size: number}
-
-// getAggregatedRepo will fetch public repositories, their languages then call worker to aggregate those data
-func getAggregatedRepo() (map[string]languageStats, error) {
-	start := time.Now()
-
-	repoDtoList, err := FetchRepositoriesList()
-	if err != nil {
-		return nil, err
-	}
-
-	done := make(chan struct{})
-	defer close(done)
-
-	repoCh := loopThroughRepo(done, *repoDtoList)
-	c := make(chan result)
-	var wg sync.WaitGroup
-	numWorker := serverConfig.WorkerNumber
-	wg.Add(numWorker)
-	for i := 0; i < numWorker; i++ {
-		go func() {
-			fetchLanguagesWorker(done, repoCh, c)
-			wg.Done()
-		}()
-	}
-	go func() {
-		wg.Wait()
-		close(c)
-	}()
-	// End of pipeline.
-
-	languagesStats := aggregateData(c)
-
-	elapsed := time.Since(start)
-
-	fmt.Println("\nfinal stats:")
-
-	for lang, stats := range languagesStats {
-		fmt.Println("\n	->", lang, "  count:", stats.count, "  size:", stats.size)
-		for name := range stats.repoList {
-			fmt.Println("		-", name)
-		}
-	}
-
-	fmt.Printf("Fetch repositories with their languages list took %s", elapsed)
-	return languagesStats, nil
+	repoList map[string]Repository
 }
 
 func aggregateData(c chan result) map[string]languageStats {
-	// RepoList use a map where key is full_name field.
-	// full_name field is unique for github
-	// repoList := make(map[string]Repository)
 	aggregatedData := make(map[string]languageStats)
 
 	for r := range c {
@@ -81,8 +25,8 @@ func aggregateData(c chan result) map[string]languageStats {
 			fmt.Println("Error detect during fetching languages from repository", r.err)
 			continue
 		}
-		// repoList[r.repository.FullName] = r.repository
 		for lang, size := range r.repository.Languages {
+			// tmpRepoList use a map where key is unique 'full_name' field.
 			var tmpRepoList map[string]Repository
 
 			if aggregatedData[lang].repoList == nil {
@@ -99,7 +43,6 @@ func aggregateData(c chan result) map[string]languageStats {
 			aggregatedData[lang] = *tmpStats
 			aggregatedData[lang].repoList[r.repository.FullName] = r.repository
 		}
-		// fmt.Println(r.repository)
 	}
 
 	return aggregatedData
@@ -153,78 +96,55 @@ func loopThroughRepo(done <-chan struct{}, repoList []RepositoryGithubDto) <-cha
 	return out
 }
 
-func merge(done <-chan struct{}, cs []<-chan Repository) <-chan Repository {
+// Pipeline:
+// 2 stages
+// 1st: list repo to chan
+// 2nd:
+//		- fetch languages
+// 		- aggregate repo: map[language: string]{count: number, size: number}
+
+// getAggregatedRepo will fetch public repositories, their languages then call worker to aggregate those data
+func getAggregatedRepo() (map[string]languageStats, error) {
+	start := time.Now()
+
+	repoDtoList, err := FetchRepositoriesList()
+	if err != nil {
+		return nil, err
+	}
+
+	done := make(chan struct{})
+	defer close(done)
+
+	repoCh := loopThroughRepo(done, *repoDtoList)
+	c := make(chan result)
 	var wg sync.WaitGroup
-	out := make(chan Repository, 1) // enough space for the unread inputs
-
-	// Start an output goroutine for each input channel in cs.  output
-	// copies values from c to out until c or done is closed, then calls wg.Done.
-	output := func(c <-chan Repository) {
-		defer wg.Done()
-		for n := range c {
-			select {
-			case out <- n:
-			case <-done:
-				return
-			}
-		}
+	numWorker := serverConfig.WorkerNumber
+	wg.Add(numWorker)
+	for i := 0; i < numWorker; i++ {
+		go func() {
+			fetchLanguagesWorker(done, repoCh, c)
+			wg.Done()
+		}()
 	}
-	wg.Add(len(cs))
-	for _, c := range cs {
-		go output(c)
-	}
-
-	// Start a goroutine to close out once all the output goroutines are
-	// done.  This must start after the wg.Add call.
 	go func() {
 		wg.Wait()
-		close(out)
+		close(c)
 	}()
-	return out
-}
+	// End of pipeline.
 
-func listRepoToTheChan(done <-chan struct{}, repoList []RepositoryGithubDto) <-chan RepositoryGithubDto {
-	out := make(chan RepositoryGithubDto, len(repoList))
-	go func() {
-		defer close(out)
-		for _, repo := range repoList {
-			select {
-			case out <- repo:
-			case <-done:
-				return
-			}
+	languagesStats := aggregateData(c)
+
+	elapsed := time.Since(start)
+
+	fmt.Println("\nfinal stats:")
+
+	for lang, stats := range languagesStats {
+		fmt.Println("\n	->", lang, "  count:", stats.count, "  size:", stats.size)
+		for name := range stats.repoList {
+			fmt.Println("		-", name)
 		}
-		close(out)
-	}()
-	return out
-}
+	}
 
-func fetchAndTransferRepoToTheChan(done <-chan struct{}, in <-chan RepositoryGithubDto) <-chan Repository {
-	out := make(chan Repository)
-
-	go func() {
-		for repoDto := range in {
-			languages, err := FetchLanguagesList(repoDto.Owner.OwnerName, repoDto.Name)
-			if err != nil {
-				return
-			}
-			repo := Repository{
-				Name:        repoDto.Name,
-				FullName:    repoDto.FullName,
-				Description: repoDto.Description,
-				OwnerName:   repoDto.Owner.OwnerName,
-				AvatarURL:   repoDto.Owner.AvatarURL,
-				Type:        repoDto.Owner.Type,
-				URL:         repoDto.URL,
-				Languages:   languages,
-			}
-			select {
-			case out <- repo:
-			case <-done:
-			}
-
-		}
-		close(out)
-	}()
-	return out
+	fmt.Printf("Fetch repositories with their languages list took %s", elapsed)
+	return languagesStats, nil
 }
